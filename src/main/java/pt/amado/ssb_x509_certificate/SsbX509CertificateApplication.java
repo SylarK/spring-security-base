92cec335de982991fa4688cfb02183acd2f6b49e
Lucas Amado <syst.lucasamado@gmail.com>
2024-10-26 19:43:39 +0100

    Base project and updating .gitignore

---
+++ src/main/java/pt/amado/ssb_x509_certificate/SsbX509CertificateApplication.java
@@ -0,0 +1,13 @@
+package pt.amado.ssb_x509_certificate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsbX509CertificateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsbX509CertificateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pt/amado/ssb_x509_certificate/SsbX509CertificateApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.amado.ssb_x509_certificate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsbX509CertificateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
