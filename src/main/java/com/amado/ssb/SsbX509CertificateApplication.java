b7ae1313a717bfcfc61d215319232190b2bac6c1
Lucas Amado <syst.lucasamado@gmail.com>
2024-10-26 19:50:32 +0100

    Base controller

--- src/main/java/com/amado/ssb/SsbX509CertificateApplication.java
+++ src/main/java/com/amado/ssb/SsbX509CertificateApplication.java
@@ -2,6 +2,8 @@ package com.amado.ssb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class SsbX509CertificateApplication {
