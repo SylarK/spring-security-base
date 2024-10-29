f2e9f74822707aa005ed063ec25d4a8906c46ebf
Lucas Amado <syst.lucasamado@gmail.com>
2024-10-29 14:07:00 +0000

    Update files
    - Create new error handling page
    - Update User.html
    - Update userController to work as expected

--- src/main/java/com/amado/ssb/controller/UserController.java
+++ src/main/java/com/amado/ssb/controller/UserController.java
@@ -1,15 +1,22 @@
 package com.amado.ssb.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/api")
+import java.security.Principal;
+
+@Controller
 public class UserController {
 
 	@GetMapping("/user")
-	public String user() {
+	public String user(Model model, Principal principal) {
+		UserDetails currentUser = (UserDetails) ((Authentication) principal).getPrincipal();
+		model.addAttribute("username", currentUser.getUsername());
 		return "user";
 	}
 
