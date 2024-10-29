e0692e7047356cc555f603279d8fd97ccbafcbf1
Lucas Amado <syst.lucasamado@gmail.com>
2024-10-29 14:29:46 +0000

    Removing unused imports

--- src/main/java/com/amado/ssb/controller/UserController.java
+++ src/main/java/com/amado/ssb/controller/UserController.java
@@ -6,20 +6,18 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.security.Principal;
 
 @Controller
 public class UserController {
 
-	@PreAuthorize("hasAuthority('ROLE_USER')")
-	@GetMapping("/user")
-	public String user(Model model, Principal principal) {
-		UserDetails currentUser = (UserDetails) ((Authentication) principal).getPrincipal();
-		model.addAttribute("username", currentUser.getUsername());
-		return "user";
-	}
+    @PreAuthorize("hasAuthority('ROLE_USER')")
+    @GetMapping("/user")
+    public String user(Model model, Principal principal) {
+        UserDetails currentUser = (UserDetails) ((Authentication) principal).getPrincipal();
+        model.addAttribute("username", currentUser.getUsername());
+        return "user";
+    }
 
 }
